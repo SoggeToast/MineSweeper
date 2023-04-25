69987bfb0c77b89484bf9433eee3410cac27bf22
MasonWendel <masonwendel@outlook.com>
2023-04-25 07:52:06 -0500

    Made the board class (Incomplete)

--- Board.java
+++ Board.java
@@ -8,10 +8,10 @@ public class Board {
     private int width;
     private int height; 
 
-     public Board(int w, int h){
+     public Board(int w, int h, int amntBombs){
         width = w;
         height = h;
-        board = createRandomBoard();
+        board = createRandomBoard(amntBombs);
      }
      // Getter and setter methods 
      public int getWidth(){
@@ -28,12 +28,37 @@ public class Board {
      }
 
      // Create board as 2d array
-     public int[][] createRandomBoard(){
+     public int[][] createRandomBoard(int amntBombs){
         int[][] newBoard = new int[height][width];
-
-        // I need to ask you how we're going to go about doing this. 
-
-
+        // KEY
+        // -1: Bomb 
+        // 0-9: Near-Bomb-Marker
+        for(int r = 0; r<height; r++){ // Puts bombs on the board. 
+            for(int c = 0; c<width;c++){
+               if(amntBombs != 0){
+                  if((int)(Math.random()+1)==0){
+                     newBoard[r][c] = -1;
+                     amntBombs--;
+                  }
+               }
+            }
+        }
+        
+        for(int r = 0; r<height; r++){
+         for(int c= 0; c<width; c++){
+            int num = newBoard[r][c];
+            if(num ==  -1){
+               for(int i = -1; i<=1;i++){
+                  for(int j = -1; j<=1;j++){
+                     if(((r+i>=0&&c+j>=0)||(r+i<height&&c+j<width))&&newBoard[r+i][c+j]!=-1){   // BRUH LOOK!! CONFUSION!!!
+                        newBoard[r+i][c+j]++;
+                     }
+                  }
+               }
+            }
+         }
+        }
+        board = newBoard; 
         return newBoard;
      }
      // Returns a formatted board that can be printed. 
