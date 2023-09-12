95249264b158ce96b9c36edab9f12efff4965ba4
SoggeToast <miguel.123.canales@gmail.com>
2023-09-11 22:11:10 -0500

    jpanel now gets user input and is able to tell which tile you press(I think)

--- GeometricShape.java
+++ GeometricShape.java
@@ -8,53 +8,63 @@ import java.awt.event.MouseEvent;
 
  
 class GeometricShape extends JPanel{
-    private int width = 500;
-    private int length = 500;       
+    public int width;
+    private static MouseEvent recentClick;
+      
+    public GeometricShape(int w){
+        width = w*50;
+    }
 
    public void MyPanel(){
         JFrame frame = new JFrame();
         frame.addMouseListener(listener);
-       frame.getContentPane().add(new GeometricShape());
+       frame.getContentPane().add(new GeometricShape(width));
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.setLocationRelativeTo(null);
-       frame.setSize(width + 10,length + 40);  
+       frame.setSize(width + 10, width + 45 + 100);  
        frame.setVisible(true);
-
-       JLabel label = new JLabel();
-       ImageIcon imageIcon = new ImageIcon("image.jpg");
-       label.setIcon(imageIcon);
-       frame.add(label);
-
    }
    
- 
    public void paint(Graphics g) {
+        //g.drawRect(100, 100, 40, 120);
+        //g.setColor(Color.green);
+        //g.fillRect(250, 250, 100, 50); 
+        g.setColor(Color.darkGray);
+        g.fillRect(0, 0, WIDTH, 100);
+        g.setColor(Color.orange);
+        g.fillRect(100, 25, 50, 50);
+        g.setColor(Color.red);
 
-
-       //g.drawRect(100, 100, 40, 120);
-       //g.setColor(Color.green);
-       //g.fillRect(250, 250, 100, 50); 
-       
-       for(int x = 0; x < length/50; x++){
-        for(int y = 0; y < width/50; y++){
-            if(y%2!=0 && x%2 == 0 || x%2 !=0 && y%2==0){
-                Color lightGreen = new Color(170, 215, 81);
-                g.setColor(lightGreen);
-            } else{
-                Color darkGreen = new Color(162, 209, 73);
-                g.setColor(darkGreen);
+        g.fillRect(200, 25, 50, 50);
+        System.out.println(width);
+        for(int x = 0; x < width/50; x++){
+            for(int y = 0; y < width/50; y++){
+                if(y%2!=0 && x%2 == 0 || x%2 !=0 && y%2==0){
+                    Color lightGreen = new Color(170, 215, 81);
+                    g.setColor(lightGreen);
+                } else{
+                    Color darkGreen = new Color(162, 209, 73);
+                    g.setColor(darkGreen);
+                }
+                g.fillRect(x*50, y*50 + 100, 50, 50);
             }
-            g.fillRect(x*50, y*50, 50, 50);
         }
-       }
-       
             
+     }
+
+     public static int getxvalue(){
+        return recentClick.getX();
+     }
+     public static int getyvalue(){
+        return recentClick.getY();
      }
 
      private MouseListener listener = new MouseListener() {
         public void mouseClicked(MouseEvent e) {
+            System.out.println(width);
             System.out.println(e);
-            
+            recentClick = e;
+            minesweeper.game.handleInput();
         }
 
         public void mousePressed(MouseEvent e) {
--- minesweeper.java
+++ minesweeper.java
@@ -1,13 +1,34 @@
+import java.util.*;
+
+import org.w3c.dom.events.MouseEvent;
+
+import java.awt.event.MouseListener;
+import java.io.*;
 import java.io.IOException;
 
 public class minesweeper{
+    public static minesweeper game = new minesweeper();
+
     public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
         //solveboard game = new solveboard("puzzel.dat", 8, 10);
         //game.begin();
 
-        GeometricShape shape = new GeometricShape();
-        shape.MyPanel();
+        System.out.print("How many squares wide do you want the board to be: ");
+        int width = input.nextInt();
 
-        
+        GeometricShape shape = new GeometricShape(width);
+        shape.MyPanel();
+        //shape.repaint();
+
+    }
+
+    public void handleInput(){
+        //System.out.println(GeometricShape.getW());
+        int x = GeometricShape.getxvalue();
+        int y = GeometricShape.getyvalue();
+        int col = (int)(x-10)/50;
+        int row = (int)(y-130)/50;
+        System.out.println(col + "        " + row);
     }
 }
